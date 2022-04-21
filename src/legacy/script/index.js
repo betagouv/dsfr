4b260397e5d06740312a9eb1e3648fecfd27384c
keryan <keryan.sanie@gmail.com>
2022-04-21 18:31:52 +0200

    fix(legacy): ajout fetch polyfill

--- src/legacy/script/index.js
+++ src/legacy/script/index.js
@@ -5,6 +5,7 @@ import './polyfill-io/add-event-listener-polyfill.min';
 import './polyfill-io/foreach-polyfill.min';
 import './polyfill-io/contains-polyfill.min';
 import './polyfill-io/matches-polyfill.min';
+import './fetch-polyfill/unfetch.umd';
 
 // import { ObjectFitPolyfill } from './object-fit-polyfill/object-fit-polyfill';
 
